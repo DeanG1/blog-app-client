bd0f8f6fc1efda8a188811f4028872b9489209b4
Dido2002 <didoguzelev@gmail.com>
2024-08-12 18:08:10 +0300

    Created functionality for login user with his credentials. Successfully getting the token

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,51 @@ import {
   FormGroup,
   Button,
 } from "reactstrap";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { loginUser } from "../services/user-service";
 const Login = () => {
+  const [loginDetail, setLoginDetail] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChange = (event, property) => {
+    setLoginDetail({ ...loginDetail, [property]: event.target.value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(loginDetail);
+    //validation
+    if (
+      loginDetail.username.trim() === "" ||
+      loginDetail.password.trim() === ""
+    ) {
+      toast.error("Username or Password  is required !!");
+      return;
+    }
+
+    //submit the data to server to generate token
+    loginUser(loginDetail)
+      .then((data) => {
+        console.log(data);
+        toast.success("Login Success");
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status === 400 || error.response.status === 404) {
+          toast.error(error.response.data.message);
+        } else {
+          toast.error("Something went wrong  on sever !!");
+        }
+      });
+  };
+  const handleReset = () => {
+    setLoginDetail({
+      username: "",
+      password: "",
+    });
+  };
   return (
     <Reusable>
       <Container>
@@ -23,20 +67,34 @@ const Login = () => {
                 <h3>Login</h3>
               </CardHeader>
               <CardBody>
-                <Form>
+                <Form onSubmit={handleFormSubmit}>
                   <FormGroup>
                     <Label for="email">Enter Email</Label>
-                    <Input type="text" id="email" />
+                    <Input
+                      type="text"
+                      id="email"
+                      onChange={(e) => handleChange(e, "username")}
+                      value={loginDetail.username}
+                    />
                   </FormGroup>
                   <FormGroup>
                     <Label for="password">Enter password</Label>
-                    <Input type="password" id="password" />
+                    <Input
+                      type="password"
+                      id="password"
+                      onChange={(e) => handleChange(e, "password")}
+                      value={loginDetail.password}
+                    />
                   </FormGroup>
                   <Container className="text-center">
                     <Button color="light" outline>
                       Login
                     </Button>
-                    <Button color="secondary" className="ms-2">
+                    <Button
+                      color="secondary"
+                      className="ms-2"
+                      onClick={handleReset}
+                    >
                       Reset
                     </Button>
                   </Container>
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -5,3 +5,9 @@ export const signUp = (user) => {
     .post("/api/v1/auth/register", user)
     .then((response) => response.data);
 };
+
+export const loginUser = (loginDetail) => {
+  return myAxios
+    .post("/api/v1/auth/login", loginDetail)
+    .then((response) => response.data);
+};
