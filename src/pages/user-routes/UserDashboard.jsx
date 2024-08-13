09b75be896623488ac19bcc4d9cb4248bb12d074
Dido2002 <didoguzelev@gmail.com>
2024-08-13 09:30:30 +0300

    Created simple private routing

---
+++ src/pages/user-routes/UserDashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>Welcome ot user dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        ducimus corrupti tenetur accusantium aut in illo enim, quod officiis!
+        Ullam magnam accusantium dolore. Deserunt repudiandae architecto
+        officiis. Provident nobis placeat sequi, quia veniam neque earum quo
+        pariatur. Provident, rem omnis!
+      </p>
+    </div>
+  );
+};
+
+export default UserDashboard;
