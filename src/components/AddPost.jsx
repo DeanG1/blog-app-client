54f3e360bdc2cf5b64928c0ec1eefbbdd48610f9
Dido2002 <didoguzelev@gmail.com>
2024-08-14 16:28:05 +0300

    Category component in progress

---
+++ src/components/AddPost.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Form,
+  Input,
+  Label,
+  Container,
+} from "reactstrap";
+import { loadAllCategories } from "../services/category-service";
+const AddPost = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    loadAllCategories()
+      .then((data) => {
+        console.log(data);
+        setCategories(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Card className="shadow-md">
+        <CardBody>
+          <h3>Whatadjaiofsdfnsdflsdnfsdnfsdfjsdfsdfsdf</h3>
+          <Form>
+            <div className="my-3">
+              <Label for="title">Post title</Label>
+              <Input
+                type="text"
+                id="title"
+                placeholder="Type here..."
+                className="rounded-0"
+              />
+            </div>
+            <div className="my-3">
+              <Label for="content">Post Content</Label>
+              <Input
+                type="textarea"
+                style={{ height: "300px" }}
+                id="content"
+                placeholder="Type here..."
+                className="rounded-0"
+              />
+            </div>
+            <div className="my-3">
+              <Label for="category">Post Category</Label>
+              <Input
+                type="select"
+                id="category"
+                placeholder="Type here..."
+                className="rounded-0"
+              >
+                {categories.map((category) => (
+                  <option key={category.categoryId}>
+                    {category.categoryTitle}
+                  </option>
+                ))}
+              </Input>
+            </div>
+
+            <Container className="text-center">
+              <Button className="rounded-0" color="primary">
+                Create Post
+              </Button>
+              <Button className="rounded-0 ms-2" color="danger">
+                Reset Content
+              </Button>
+            </Container>
+          </Form>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default AddPost;
--- src/pages/user-routes/UserDashboard.jsx
+++ src/pages/user-routes/UserDashboard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Reusable from "../../components/Reusable";
-
+import AddPost from "../../components/AddPost";
+import { Container } from "reactstrap";
 const UserDashboard = () => {
   return (
     <Reusable>
-      <h1>Welcome ot user dashboard</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-        ducimus corrupti tenetur accusantium aut in illo enim, quod officiis!
-        Ullam magnam accusantium dolore. Deserunt repudiandae architecto
-        officiis. Provident nobis placeat sequi, quia veniam neque earum quo
-        pariatur. Provident, rem omnis!
-      </p>
+      <Container>
+        <AddPost />
+      </Container>
     </Reusable>
   );
 };
