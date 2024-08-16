0a02b6d1a5baf360f394a1c3449d980bbe41bc1e
Dido2002 <didoguzelev@gmail.com>
2024-08-16 10:50:00 +0300

    Working on paginaton

--- src/components/NewFeed.jsx
+++ src/components/NewFeed.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { loadAllPosts } from "../services/post-service";
-import { Row, Col } from "reactstrap";
+import {
+  Row,
+  Col,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Container,
+} from "reactstrap";
 import Post from "./Post";
+import { toast } from "react-toastify";
 function NewFeed() {
   const [postContent, setPostContent] = useState({
     content: [],
@@ -14,16 +22,29 @@ function NewFeed() {
   });
 
   useEffect(() => {
-    loadAllPosts()
-      .then((data) => {
-        console.log(data);
-        setPostContent(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    changePage(0);
+    // loadAllPosts(0, 5)
+    //   .then((data) => {
+    //     console.log(data);
+    //     setPostContent(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //     toast.error("Error in loading posts");
+    //   });
   }, []);
 
+  const changePage = (pageNumber = 0, pageSize = 5) => {
+    loadAllPosts(pageNumber, pageSize)
+      .then((data) => {
+        setPostContent(data);
+        console.log(data);
+        window.scroll(0, 0);
+      })
+      .catch((error) => {
+        toast.error("Error in loading posts");
+      });
+  };
   return (
     <div className="container-fluid">
       <Row>
@@ -32,6 +53,31 @@ function NewFeed() {
           {postContent.content.map((post) => (
             <Post post={post} key={post.postId} />
           ))}
+          <Container className="mt-3">
+            <Pagination size="lg">
+              <PaginationItem
+                onClick={() => changePage(--postContent.pageNumber)}
+                disabled={postContent.pageNumber === 0}
+              >
+                <PaginationLink previous>Previous</PaginationLink>
+              </PaginationItem>
+              {[...Array(postContent.totalPages)].map((item, index) => (
+                <PaginationItem
+                  onClick={() => changePage(index)}
+                  active={index === postContent.pageNumber}
+                  key={index}
+                >
+                  <PaginationLink>{index + 1}</PaginationLink>
+                </PaginationItem>
+              ))}
+              <PaginationItem
+                onClick={() => changePage(++postContent.pageNumber)}
+                disabled={postContent.lastPage}
+              >
+                <PaginationLink next>Next</PaginationLink>
+              </PaginationItem>
+            </Pagination>
+          </Container>
         </Col>
       </Row>
     </div>
--- src/pages/user-routes/UserDashboard.jsx
+++ src/pages/user-routes/UserDashboard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Reusable from "../../components/Reusable";
 import AddPost from "../../components/AddPost";
 import { Container } from "reactstrap";
-import NewFeed from "../../components/NewFeed";
 const UserDashboard = () => {
   return (
     <Reusable>
